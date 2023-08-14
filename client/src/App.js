78f1318cb4489a15c10b2309314400c9fb20c498
Brodie Kime <brodie.kime@gmail.com>
2023-08-14 19:06:13 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import EditProfile from "./pages/EditProfile";
+import CreateSample from "./pages/CreateSample";
+import BrowseSamples from "./pages/BrowseSamples";
+import CreatePack from "./pages/CreatePack";
+import PackDetails from "./pages/PackDetails";
+import BrowsePacks from "./pages/BrowsePacks";
+import BrowseProfile from "./pages/BrowseProfile";
+
+const App = () => {
+  const getToken = () => {
+    return localStorage.getItem("authToken");
+  };
+
+  const LoggedIn = () => {
+    return getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
+
+  const NotLoggedIn = () => {
+    return !getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse-samples" element={<BrowseSamples />} />
+        <Route path="/browse-packs" element={<BrowsePacks />} />
+        <Route path="/pack-details/:id" element={<PackDetails />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/browse-profile/:id" element={<BrowseProfile />} />
+
+        <Route element={<LoggedIn />}>
+          <Route path="/edit-profile/:id" element={<EditProfile />} />
+          <Route path="/create-sample" element={<CreateSample />} />
+          <Route path="/create-pack" element={<CreatePack />} />
+        </Route>
+
+        <Route element={<NotLoggedIn />}>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
